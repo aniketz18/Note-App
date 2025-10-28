d5def809e19d550efaf373269d11ac4b56856f60
aniketz18 <aniketzade700@gmail.com>
2025-10-28 22:16:06 +0530

    fixed get , post methods

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const path = require("path");
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 const fs = require("fs");
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -11,8 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  fs.readdir(`./files`, function (err, files) {
-    res.render("index", { files: files });
+  fs.readdir("./files", (err, files) => {
+    if (err) {
+      console.error("Error reading files folder:", err);
+      files = []; // fallback if folder missing or error
+    }
+    res.render("index", { files });
   });
 });
 
@@ -21,20 +25,21 @@ app.get("/files/:filename", (req, res) => {
     `files/${req.params.filename}`,
     "utf-8",
     function (err, filedata) {
-      res.render("show",{filename: req.params.filename,data :filedata } );
+      res.render("show", { filename: req.params.filename, data: filedata });
       console.log(filedata);
     }
   );
 });
 
 app.post("/create", (req, res) => {
-  console.log(req.body);
-  fs.writeFileSync(
-    `./files/${req.body.title.split(" ").join("")}.txt`,
-    `${req.body.info}`,
-    function (error) {}
-  );
-  res.redirect("/");
+  const fileName = req.body.title.split(" ").join("") + ".txt";
+  const filePath = `./files/${fileName}`;
+  fs.writeFile(filePath, req.body.info, (err) => {
+    if (err) console.error("Error writing file:", err);
+    res.redirect("/");
+  });
 });
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
